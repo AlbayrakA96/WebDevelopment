b7d154711ca62bbb8b5dcc39ba0ec8f4f7a6c570
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-02-03 18:43:30 +0100

    V1.1 Portfolio

---
+++ Week 5/Portfolio/main.js
@@ -0,0 +1,31 @@
+// Navbar button
+$(document).ready(function(){
+    $('.btn').click(function(){
+      $('.items').toggleClass("show");
+      $('ul li').toggleClass("hide");
+    });
+  });
+
+
+// Hero typer voor banner
+let heroTyper = document.getElementById('hero__typer');
+var typewriter = new Typewriter(heroTyper, {
+  loop: true
+});
+typewriter.typeString("Hello, I'm Abdusselam Albayrak.")
+  .pauseFor(500)
+  .deleteChars(20)
+  .typeString("<strong>a Front-end Developer.</strong>")
+  .pauseFor(1500)
+  .deleteAll()
+  .typeString("<strong>Abdusselam Albayrak.</strong>")
+  .pauseFor(1533300)
+//   .typeString('Strings can be removed')
+//   .pauseFor(2500)
+//   .deleteChars(7)
+//   .typeString('<strong>altered!</strong>')
+//   .pauseFor(2500).
+  .deleteAll()
+  .start();
+  
+  
