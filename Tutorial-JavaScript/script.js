b912b5bf912b1d7e6e5d94e6368ac2be295bcc0b
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-02-15 08:58:57 +0100

    JavaScript recap

---
+++ Tutorial-JavaScript/script.js
@@ -0,0 +1,19 @@
+/* let firstName = "Abdusselam";
+console.log(firstName);
+
+let country = "Netherlands";
+let continent = "Europe";
+let population = 16000000;
+console.log(typeof population);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); */
+
+let age = 30;
+age = 31;
