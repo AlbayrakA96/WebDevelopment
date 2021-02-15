8e45ae240bf60c90b1ac3d226d56dca9befdd0ca
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-02-15 20:31:24 +0100

    JavaScript Challenge #2

--- Tutorial-JavaScript/script.js
+++ Tutorial-JavaScript/script.js
@@ -55,20 +55,66 @@ console.log(x); */
 
 // Strings and Template literals -----------------------------------------------------------
 
-const firstName = "Abdusselam";
-const job = "front-end developer";
-const birthYear = 1996;
-const currentYear = 2021;
+// const firstName = "Abdusselam";
+// const job = "front-end developer";
+// const birthYear = 1996;
+// const currentYear = 2021;
 
-const abdusInfo =
-  "I'm " +
-  firstName +
-  ", a " +
-  (currentYear - birthYear) +
-  "years old " +
-  job +
-  "!";
-console.log(abdusInfo);
+// const abdusInfo =
+//   "I'm " +
+//   firstName +
+//   ", a " +
+//   (currentYear - birthYear) +
+//   "years old " +
+//   job +
+//   "!";
+// console.log(abdusInfo);
 
-const abdusInfoNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
-console.log("abdusInfoNew:", abdusInfoNew);
+// const abdusInfoNew = `I'm ${firstName}, a ${
+//   currentYear - birthYear
+// } year old ${job}!`;
+// console.log(`Just a regular string`);
+// console.log(`String
+// multiple
+// lines`);
+
+// Taking decicisions if / else statemens----------------------------------------------------------------
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 👍");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young, wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 2012;
+
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(`${century} century`);
+
+// Coding Challange #2 ----------------------------------------------------------------
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI ${BMIMark} is higer than John's ${BMIJohn}`);
+}
