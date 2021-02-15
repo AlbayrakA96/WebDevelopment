053a6e8c64cf6fe3859ffc8805a375865febffdf
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-02-15 17:01:08 +0100

    JavaScript Challenge #1

--- Tutorial-JavaScript/script.js
+++ Tutorial-JavaScript/script.js
@@ -1,19 +1,74 @@
-/* let firstName = "Abdusselam";
-console.log(firstName);
+// Math operators-------------------------------
+// const currentYear = 2021;
+// const ageAbdus = currentYear - 1996;
+// const ageFriend = currentYear - 1999;
+// console.log(ageAbdus, ageFriend);
 
-let country = "Netherlands";
-let continent = "Europe";
-let population = 16000000;
-console.log(typeof population);
+// console.log(ageAbdus * 2, ageAbdus / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-let year;
-console.log(year);
-console.log(typeof year);
+// const firstName = "Abdusselam";
+// const lastName = "Albayrak";
+// console.log(firstName + " " + lastName);
 
-year = 1991;
-console.log(typeof year);
+// Assignment operators---------------------------
+/* let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
 
-console.log(typeof null); */
+console.log(x); */
 
-let age = 30;
-age = 31;
+// Comparison operators-----------------------------
+
+// console.log(ageAbdus > ageFriend); // >, <, >=, <=
+// console.log(ageFriend >= 18);
+
+// const isFullAge = ageAbdus >= 18;
+// console.log("isFullAge:", isFullAge);
+
+// Operators precedence ---------------------------
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageAbdus + ageFriend) / 2;
+// console.log("averageAge:", averageAge);
+
+// Coding Challange #1 ----------------------------------------------------------------
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// Strings and Template literals -----------------------------------------------------------
+
+const firstName = "Abdusselam";
+const job = "front-end developer";
+const birthYear = 1996;
+const currentYear = 2021;
+
+const abdusInfo =
+  "I'm " +
+  firstName +
+  ", a " +
+  (currentYear - birthYear) +
+  "years old " +
+  job +
+  "!";
+console.log(abdusInfo);
+
+const abdusInfoNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log("abdusInfoNew:", abdusInfoNew);
