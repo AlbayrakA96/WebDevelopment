5962fe0bf4ae676148c668b3a97f54e691e04f86
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-02-16 15:58:14 +0100

    JavaScript Challenge #3

--- Tutorial-JavaScript/script.js
+++ Tutorial-JavaScript/script.js
@@ -100,21 +100,140 @@ console.log(x); */
 // console.log(`${century} century`);
 
 // Coding Challange #2 ----------------------------------------------------------------
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 // const massMark = 95;
 // const heightMark = 1.88;
 // const massJohn = 85;
 // const heightJohn = 1.76;
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+// console.log(BMIMark, BMIJohn, markHigherBMI);
 
-if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI ${BMIMark} is higer than John's ${BMIJohn}`);
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI ${BMIMark} is higer than John's ${BMIJohn}`);
+// } else {
+//   console.log(`John's ${BMIJohn} is higer than Mark's BMI ${BMIMark}`);
+// }
+
+// Type Conversion and Coercion--------------------------------------
+// type conversion ------------
+// const inputYear = "1991";
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Jonas"));
+// // NaN is Not a Number / invalid number
+
+// console.log(String(23), 23);
+
+// // type coercion ---------------
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+
+// let n = "1" + 1; // '11'
+// n = n - 1;
+// console.log(n);
+
+// Truthy and Falsy values --------------------------------------------
+// 5 Falsy values: 0, '', undefined, null, NaN,
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+// Equality Operators ()== vs. ===) --------------------------------
+
+// const age = "18";
+// if (age === 18) console.log("You just became an adult :D (strict)");
+
+// if (age == 18) console.log("You just became an adult :D (loose)");
+
+// const favourite = Number(prompt("What's your favourite number?"));
+// console.log(typeof favourite);
+
+// if (favourite === 28) {
+//   // '28' == 28
+//   console.log("Cool! 28 is an amazing number");
+// } else if (favourite === 7) {
+//   console.log("7 is also a cool number");
+// } else if (favourite === 9) {
+//   console.log("9 is also a cool number");
+// } else {
+//   console.log("Number is not 28 or 7");
+// }
+
+// if (favourite !== 28) console.log("Why not 28?");
+
+// Logical operators -------------------------------------------------------
+
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+
+// console.log(hasDriversLicense && hasGoodVision); // And
+// console.log(hasDriversLicense || hasGoodVision); // Or
+// console.log(!hasDriversLicense);
+
+// // const shouldDrive = hasDriversLicense && hasGoodVision; //
+
+// // if (hasDriversLicense && hasGoodVision) {
+// //   console.log("Sarah is able to drive!");
+// // } else {
+// //   console.log("Someone else should drive...");
+// // }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+// Coding Challenge #3 ---------------------------------------------------
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks �
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+const averageDolphins = (96 + 108 + 89) / 2;
+const averageKoalas = (88 + 91 + 110) / 2;
+
+if (averageDolphins > averageKoalas) {
+  console.console.log();
 }
