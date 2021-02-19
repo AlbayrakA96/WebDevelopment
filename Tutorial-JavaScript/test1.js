0885039f3294d7b26b1a02343686c148710a91f0
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-02-19 17:21:41 +0100

    V1.4 React tutorial

---
+++ Tutorial-JavaScript/test1.js
@@ -0,0 +1,12 @@
+// Const manupileren
+
+const users = ["Abdusselam", "Yassin", "Dave"];
+
+users.push("Niek");
+console.log(users);
+
+// inheritance
+
+const salaris = 100;
+const salarisTotaal = salaris + 50;
+console.log("salarisTotaal:", salarisTotaal);
--- tutorial-react/src/App.js
+++ tutorial-react/src/App.js
@@ -2,44 +2,41 @@ import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = (props) => {
+const app = () => {
   const [personsState, setPersonsState] = useState({
     persons: [
-      { name: "Abdusselam", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 },
+      { name: "Anthony Joshua", age: 31 },
+      { name: "Tyson Fury", age: 31 },
+      { name: "Mike Tyson", age: 55 },
     ],
   });
+  const [otherState, setOtherState] = useState("some other value");
+
+  console.log(personsState, otherState);
 
   const switchNameHandler = () => {
-    //console.log("Was clicked!");
-    //this.state.persons[0].name = "Abdusselam";]
     setPersonsState({
       persons: [
-        { name: "Max", age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 27 },
+        { name: "Floyd Mayweather", age: 43 },
+        { name: "Gervonta Davis", age: 24 },
+        { name: "David Haney", age: 21 },
       ],
+      otherState: personsState.otherState,
     });
   };
 
   return (
     <div className="App">
-      <h1>Hello, I'm a front-end developer</h1>
-      <p>Learning React.Js</p>
+      <h1>Welcome to my React App!</h1>
+      <p>This is really working!</p>
       <button onClick={switchNameHandler}>Switch Name</button>
       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
-        My hobbies: Racing
+        My Hobbies: Boxing
       </Person>
       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
   );
-  // return React.createElement(
-  //   "div",
-  //   { className: "App" },
-  //   React.createElement("h1", null, "Does this work?")
-  // );
 };
 
 export default app;
