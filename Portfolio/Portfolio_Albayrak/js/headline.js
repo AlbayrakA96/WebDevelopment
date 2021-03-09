c505d89cc6267b42a86f9fa5d79739a2db9cdf61
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-03-09 16:51:08 +0100

    Updated Webdevelopment folder
    deleted test apps
    started weather app

--- Portfolio/Portfolio_Albayrak/js/headline.js
+++ Portfolio/Portfolio_Albayrak/js/headline.js
@@ -16,6 +16,8 @@ jQuery(document).ready(function($){
 	
 	initHeadline();
 	
+	
+
 
 	function initHeadline() {
 		//insert <i> element for each letter of a changing word
---
+++ Projects_with_React/weather-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h2 className="title">
+          <i className="fa fa-cloud"></i>Weather App
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
