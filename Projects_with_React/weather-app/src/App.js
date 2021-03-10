f59ee248c1b01fb0cf773ed87924cba0bfd82a50
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-03-10 16:51:04 +0100

    WeatherApp (started)
    Input field, button added to weatherapp

--- Projects_with_React/weather-app/src/App.js
+++ Projects_with_React/weather-app/src/App.js
@@ -8,6 +8,10 @@ function App() {
         <h2 className="title">
           <i className="fa fa-cloud"></i>Weather App
         </h2>
+        <div className="search-form">
+          <input type="text" placeholder="Voer uw stad in" />
+          <button type='button'>Search</button>
+        </div>
       </div>
     </div>
   );
