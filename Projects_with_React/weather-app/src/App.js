69c174b010ae13aa9ed5a0c26983e44d134a5017
Abdusselam Albayrak <albayraksafety@gmail.com>
2021-03-13 23:03:03 +0100

    React projects

--- Projects_with_React/weather-app/src/App.js
+++ Projects_with_React/weather-app/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import WeatherCard from "./Components/WeatherCard/WeatherCard";
-import WeatherDetail from './Components/WeatherDetail/WeatherDetail'
+import WeatherDetail from "./Components/WeatherDetail/WeatherDetail";
 const App = () => {
-
   return (
     <div className="App">
-      <WeatherCard city="Dubai" />
-      <WeatherCard city="Amsterdam" />
-      <WeatherCard city="Las Vegas" />
-      <WeatherDetail />
+      <WeatherCard city="istanbul" />
+      <WeatherCard city="bali" />
+      <WeatherCard city="new york" />
+      {/* <WeatherCard city="Amsterdam" /> */}
+      {/* <WeatherDetail /> */}
     </div>
   );
 };
