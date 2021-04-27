8f776824fc20bd1e0f4caf1f561238f8008d56a7
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-04-27 19:08:03 +0200

    coding challenge #4

--- Tutorial-JavaScript/script.js
+++ Tutorial-JavaScript/script.js
@@ -300,3 +300,11 @@ console.log(description);
 // else {
 //     console.log(`There is no winner! SCORE IS BELOW: 100, Koalas:${avKoalas}, Dolphins:${avDolphins}`);
 // }
+
+// Coding Challenge #4 ------------------------------
+
+// const bill = 400;
+// const tip15 = bill * 0.15;
+// const tip20 = bill * 0.20;
+// const tip = bill >= 50 && bill <= 300 ? tip15 : tip20;
+// console.log(`The bill was $${bill},- the tip was $${tip}, and the total value = ${bill + tip},-`);
