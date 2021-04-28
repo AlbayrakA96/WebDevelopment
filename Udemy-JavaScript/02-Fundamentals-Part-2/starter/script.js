6c9eeca20d509aca86c3cfc0e4b26ba585dfca95
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-04-28 17:02:47 +0200

    functions

---
+++ Udemy-JavaScript/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,36 @@
+// "use strict";
+
+// Strictmode
+
+/* let hasDriversLicense = false;
+ const passTest = true;
+ if (passTest) hasDriversLicense = true;
+ if (hasDriversLicense) console.log("i can drive"); */
+
+/* Functions --------------------------------------- (the process of using a function is called invoking, running or calling the function) */
+
+/*
+function logger() {
+  console.log("My name is Abdusselam");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// Function assignment 
+function describeCountry(country, population, capitalCity) {
+    const info = `${country} has ${population}M. people and its capital city is ${capitalCity}.`;
+    return info;
+}
+const turkije = describeCountry('Turkey', 18, 'Istanbul');
+console.log(turkije); */
