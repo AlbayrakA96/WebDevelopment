8331394d4f8b3592e38794c67f73ca8587365399
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-04-28 17:54:43 +0200

    lectures & assignments Functions

--- Udemy-JavaScript/02-Fundamentals-Part-2/starter/script.js
+++ Udemy-JavaScript/02-Fundamentals-Part-2/starter/script.js
@@ -34,3 +34,48 @@ function describeCountry(country, population, capitalCity) {
 }
 const turkije = describeCountry('Turkey', 18, 'Istanbul');
 console.log(turkije); */
+
+// Function declaration vs expression
+/* function calcAge1(birthYear) {
+  return 2021 - birthYear;
+}
+const age1 = calcAge1(1996);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+};
+const age2 = calcAge2(1996);
+
+console.log(age2, age1); */
+/*
+function percentageOfWorld1(country, population) {
+    const perc = Math.round((population / 7900) * 100);
+    return `${country} has ${population} Million people, its about ${perc}percent of the world`;
+}
+const turkey = percentageOfWorld1("Turkey", 1441);
+const china = percentageOfWorld1("China", 3441);
+const iraq = percentageOfWorld1("Iraq", 2441);
+console.log(turkey);
+console.log(china);
+console.log(iraq);
+const percentageOfWorld2 = function (country, population) {
+    const perc = Math.round((population / 7900) * 100);
+    return `${country} has ${population} Million people, its about ${perc}percent of the world`;
+}
+const country2 = percentageOfWorld2("Turkey", 2556);
+console.log(country2);
+// ${Naam} heeft een ${schoenmaat}.
+function kledingDetail(naam, schoenmaat) {
+    return `${naam} heeft een schoenmaat van:${schoenmaat}.`
+}
+// Functions declaration vs expression  
+
+const man = kledingDetail("Abduss", 45);
+console.log(man);
+// expression
+const kledingDetails = function (naam, schoenmaat) {
+    return `${naam} heeft een schoenmaat van: ${schoenmaat}.`
+}
+const man1 = kledingDetails("Sayf", 41);
+console.log(man1); */
