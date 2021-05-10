3b3eace2a8ff38e35aa57d68fcbef849bc11f25c
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-05-10 19:17:57 +0200

    FINISHED: Challenge #1 DOM and event

--- Udemy-JavaScript/05-Guess-My-Number/starter/script.js
+++ Udemy-JavaScript/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,67 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = 10;
+// document.querySelector('.score').textContent = 50;
+// document.querySelector('.guess').value = 50;
+
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(randomNumber);
+let score = 20;
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.backgroundColor = '15rem';
+  //   document.getElementById('body').reset();
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Dit is geen valide nummer!';
+    // When player wins
+  } else if (guess === randomNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = randomNumber;
+    document.querySelector('body').style.backgroundColor = 'Green';
+
+    document.querySelector('.number').style.width = '30rem';
+    // When guess is to high
+  } else if (guess > randomNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Te hoog!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('body').style.backgroundColor = 'Orange';
+    } else {
+      document.querySelector('.message').textContent = 'Je heb verloren!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'Red';
+    }
+    // When guess is to low
+  } else if (guess < randomNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Te laag!';
+      score--;
+      document.querySelector('.score').textContent = score;
+
+      document.querySelector('body').style.backgroundColor = 'Orange';
+    } else {
+      document.querySelector('.message').textContent = 'Je heb verloren!';
+      document.querySelector('.score').textContent = 0;
+
+      document.querySelector('body').style.backgroundColor = 'Red';
+    }
+  }
+});
