cd445d0d78560e0598264012894efd8aba0243e9
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-05-14 22:57:53 +0200

    ADD: DRY

--- Udemy-JavaScript/05-Guess-My-Number/starter/script.js
+++ Udemy-JavaScript/05-Guess-My-Number/starter/script.js
@@ -67,3 +67,63 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+// let randomNumber = Math.trunc(Math.random() * 20) + 1;
+// console.log(randomNumber);
+// let score = 20;
+// let highScore = 0;
+
+// const displayMessage = function (message) {
+//   document.querySelector('.message').textContent = message;
+// };
+
+// // Reset Button -----------------------------------------------
+
+// document.querySelector('.again').addEventListener('click', function () {
+//   randomNumber = Math.trunc(Math.random() * 20) + 1;
+//   console.log(randomNumber);
+//   score = 20;
+//   document.querySelector('.guess').value = '';
+
+//   document.querySelector('.number').textContent = '?';
+//   document.querySelector('.score').textContent = score;
+//   displayMessage('Start guessing...');
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   document.querySelector('.number').style.width = '15rem';
+//   //   document.getElementById('body').reset();
+// });
+
+// // Submit Button ------------------------------------------------
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+
+//   // When there is no input
+//   if (!guess) {
+//     displayMessage('Dit is geen valide nummer!');
+//     // When player wins
+//   } else if (guess === randomNumber) {
+//     displayMessage('Correct Number!');
+//     document.querySelector('.number').textContent = randomNumber;
+//     document.querySelector('body').style.backgroundColor = 'Green';
+
+//     document.querySelector('.number').style.width = '30rem';
+
+//     if (score > highScore) {
+//       highScore = score;
+//       document.querySelector('.highscore').textContent = highScore;
+//     }
+
+//     // When guess is wrong
+//   } else if (guess !== randomNumber) {
+//     if (score > 1) {
+//       displayMessage(guess > randomNumber ? 'Te hoog' : 'Te laag');
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//       document.querySelector('body').style.backgroundColor = 'Orange';
+//     } else {
+//       displayMessage('Je heb verloren!');
+//       document.querySelector('.score').textContent = 0;
+//       document.querySelector('body').style.backgroundColor = 'Red';
+//     }
+//   }
+// });
