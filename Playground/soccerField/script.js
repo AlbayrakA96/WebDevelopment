aea147638ce3470c4dd52bffd0aea192f48575b6
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-05-19 22:09:58 +0200

    ADDED: - Shoot button - Reset button

--- Playground/soccerField/script.js
+++ Playground/soccerField/script.js
@@ -1,8 +1,54 @@
-function voetbal()
-{
-    document.getElementById("voetbal").style.top='-280px';
-    document.getElementById("voetbal").style.left='500px';
-}
+"use strict";
 
-console.log(voetbal);
-voetbal.style.marginLeft = '100px';
+const shootButton = document.getElementById("game--shoot");
+const gameVoetbal = document.getElementById("game--voetbal");
+
+// Goal Text
+const goalText = document.getElementById("game--goalText");
+const scoreBord = document.getElementById("game--scoreBord");
+const resetGame = document.getElementById("game--reset");
+
+let score = 0;
+let text = "";
+let playing = true;
+
+
+resetGame.addEventListener("click",function() {
+    scoreBord.textContent = 0;
+    goalText.classList.add("hidden");
+})
+
+
+shootButton.addEventListener("click", function () {
+  const schietCoord1 = Math.trunc(Math.random() * 6) + 1;
+  const schietCoordH = Math.trunc(Math.random() * 76) + 175;
+  const schietCoordB = Math.trunc(Math.random() * 228) + 300;
+  const missenCoordH = Math.trunc(Math.random() * 51) + 290;
+  if (playing) {
+    if (schietCoord1 > 3) {
+      gameVoetbal.style.bottom = `${schietCoordH}px`;
+      gameVoetbal.style.left = `${schietCoordB}px`;
+      goalText.classList.remove("hidden");
+      goalText.textContent = "GOAALLLLLLLLLL";
+
+      score++;
+      scoreBord.textContent = score;
+      if (score === 5) {
+        goalText.textContent = "GEWONNEN";
+        // goalText.classList.add("hidden");
+        resetFunction();
+        playing = false;
+      } 
+      console.log(score);
+    } else {
+      gameVoetbal.style.bottom = `${missenCoordH}px`;
+      gameVoetbal.style.left = `${schietCoordB}px`;
+      goalText.classList.remove("hidden");
+      goalText.textContent = "MISSSSSSSSS";
+    }
+  }
+});
+
+// 150
+
+// missen bottom 220px minimaal 330maximaal
