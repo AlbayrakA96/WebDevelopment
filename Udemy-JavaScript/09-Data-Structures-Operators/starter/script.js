2e5aedfa2ab9139156fff27a1fc1264486e579f2
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-05-23 23:48:10 +0200

    Array structure

--- Udemy-JavaScript/09-Data-Structures-Operators/starter/script.js
+++ Udemy-JavaScript/09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,11 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z, f] = restaurant.categories;
+console.log(x, y, z, f);
