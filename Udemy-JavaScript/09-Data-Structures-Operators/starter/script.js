5174923e2910ae398f6d93e3a8318204096422a5
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-05-23 23:55:31 +0200

    NEW: ARRAY DESTRUCTERING

--- Udemy-JavaScript/09-Data-Structures-Operators/starter/script.js
+++ Udemy-JavaScript/09-Data-Structures-Operators/starter/script.js
@@ -30,8 +30,17 @@ const restaurant = {
 
 const arr = [2, 3, 4];
 const a = arr[0];
-const b = arr[1];
-const c = arr[2];
 
 const [x, y, z, f] = restaurant.categories;
-console.log(x, y, z, f);
+// console.log('categories:', restaurant.categories)
+// console.log(x, y, z, f);
+
+let [main, , secondary] = restaurant.categories
+console.log(main, secondary)
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[secondary, main]
